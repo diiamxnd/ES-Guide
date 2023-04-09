7c7b42010905eb42f73d50b2eb6e07a4c83e4db4
EN | Ghost <ru5ki0day@yandex.ru>
2023-04-09 15:24:04 +0300

    Added text
    > Changed logo name
    > Changed text in Introduction
    > Added github & discord links

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DarkBot Guide</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/darkbot-reloaded/Darkbot',
   },
   chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+    link: 'https://discord.gg/darkbot',
   },
   i18n: [
     { locale: 'en', text: 'English' },
